98a9f45b038e5d6ce49780602fcf8af7958b4b07
Yağız <93614198+YagizSahin@users.noreply.github.com>
2023-01-27 02:46:26 +0300

    views folder created

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const express = require('express');
+const ejs=require('ejs');
 const path = require('path');
 
 const app = express();
-
+//template engine
+app.set("view engine","ejs");
+//middleware
 app.use(express.static('public'));
 
+//routes
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
 });
-app.get('/about.html', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/about.html'));
+app.get('/about', (req, res) => {
+  res.render('about');
 });
-app.get('/contact.html', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/contact.html'));
+app.get('/add', (req, res) => {
+  res.render('add');
 });
+
+
 
 
 const port = 3000;
